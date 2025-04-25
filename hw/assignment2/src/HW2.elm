563112f07c19eb44f36ab0058f069e9e37d6c5a2
Nadir Isweesi <isweesin@oregonstate.edu>
2025-04-24 17:10:17 -0700

    Problem 2 B

--- hw/assignment2/src/HW2.elm
+++ hw/assignment2/src/HW2.elm
@@ -9,38 +9,38 @@ module HW2 exposing (..)
 -- Problem 1
 
 -- A
-type alias Name = String
-type alias Num = Int
+-- type alias Name = String
+-- type alias Num = Int
 
-type alias Pars = List Name
-type alias Vals = List Num
+-- type alias Pars = List Name
+-- type alias Vals = List Num
 
-type Cmd 
-        = Pen Mode
-        | MoveTo Pos Pos
-        | Def Name Pars Cmd
-        | Call Name Vals
-        | Seq Cmd Cmd
+-- type Cmd 
+--         = Pen Mode
+--         | MoveTo Pos Pos
+--         | Def Name Pars Cmd
+--         | Call Name Vals
+--         | Seq Cmd Cmd
 
-type Mode 
-        = Up
-        | Down
+-- type Mode 
+--         = Up
+--         | Down
 
-type Pos
-        = NumPos Num
-        | NamedPos Name
+-- type Pos
+--         = NumPos Num
+--         | NamedPos Name
 
 -- B
-vector : Cmd
-vector =
-        Def "vector" ["x1", "y1", "x2", "y2"]
-            (Seq
-                (Pen Down)
-                (Seq
-                    (MoveTo (NamedPos "x1") (NamedPos "y1"))
-                    (MoveTo (NamedPos "x2") (NamedPos "y2"))
-                )
-            )
+-- vector : Cmd
+-- vector =
+--         Def "vector" ["x1", "y1", "x2", "y2"]
+--             (Seq
+--                 (Pen Down)
+--                 (Seq
+--                     (MoveTo (NamedPos "x1") (NamedPos "y1"))
+--                     (MoveTo (NamedPos "x2") (NamedPos "y2"))
+--                 )
+--             )
 
 -- Problem 2
 
@@ -49,16 +49,41 @@ vector =
 type alias NonTerm = String
 type alias Term = String
 
-type Grammar = Grammar (List Prod)
+type alias Grammar = List Prod
 
-type Prod = (NonTerm, List Rhs)
+type alias Prod = (NonTerm, List Rhs)
 
 type Rhs = Rhs (List Symbol)
 
 type Symbol = Nt NonTerm
             | T Term
 
+
 -- B
+
+condP : Prod
+condP = 
+        ( "cond",
+        [ Rhs [T "T"]
+        , Rhs [T "not", Nt "cond"]
+        , Rhs [T "(", Nt "cond", T ")"]
+        ]
+        )
+
+stmtP : Prod
+stmtP = 
+        ( "stmt",
+        [ Rhs [T "skip"]
+        , Rhs [T "while", Nt "cond", T "do", T "{", Nt "stmt", T "}"]
+        , Rhs [Nt "stmt", T ";", Nt "stmt"]
+        ]
+        )
+
+imp : Grammar
+imp = 
+    [ condP
+    , stmtP
+    ]
 
 -- C
 
