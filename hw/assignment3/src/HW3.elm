1d34068a269386c9c3eb401f94b8bf080a0e7e59
mortonwi <135153158+mortonwi@users.noreply.github.com>
2025-05-02 12:17:32 -0700

    Finished HW3

--- hw/assignment3/src/HW3.elm
+++ hw/assignment3/src/HW3.elm
@@ -32,17 +32,49 @@ semOp op stack = case op of
                  _        -> Nothing
 
 semProg : Prog -> D
--- could not figure out how to remove the stack parameter requirement
 semProg prog stack =
     case prog of
         [] ->
             Just stack
 
-        -- recursively evaluates the operations and modifies stack
+        -- Recursively evaluates the operations and modifies stack
         op::rs ->
             case semOp op stack of
+                -- Handles errors
                 Nothing ->
                     Nothing
 
+                -- Handles operations
                 Just newStack ->
                     semProg rs newStack
+
+-- 2. Mini Logo
+-- Abstract Syntax
+type alias Point = (Int,Int)
+type Mode = Up | Down
+type Cmd = Pen Mode | MoveTo Point | Seq Cmd Cmd
+type alias State = (Mode,Point)
+
+-- Semantic Domain
+type alias Line = (Point,Point)
+type alias Lines = List Line
+
+-- Semantic Functions
+semCmd : Cmd -> State -> (State,Lines)
+semCmd cmd (mode, point) = case cmd of
+           Pen m     -> ((m, point), [])           -- Set mode
+           MoveTo p  -> case mode of               -- Move to point base on mode
+                        Down -> ((mode, p), [(point, p)])
+                        Up   -> ((mode, p), [])
+           Seq c1 c2 -> let                        -- Recursively solve and combine
+                                (state1, lines1) = semCmd c1 (mode, point)
+                                (state2, lines2) = semCmd c2 state1
+                        in
+                                (state2, lines1 ++ lines2)
+
+lines : Cmd -> Lines
+lines cmd = let
+                initState = (Up, (0,0))                 -- Set initial state 
+                (_, drawnLines) = semCmd cmd initState  -- Build on initState
+            in
+                drawnLines
