2f728a4ae1b5b0fcdb50b9eb986070c6e95a93f7
Nadir Isweesi <isweesin@oregonstate.edu>
2025-05-19 22:17:48 -0700

    assignment 4 part 1

--- hw/assignment4/src/HW4.elm
+++ hw/assignment4/src/HW4.elm
@@ -8,23 +8,34 @@ module HW4 exposing (..)
 -- 1. Stack Language
 
 -- A
+-- stack operations
 type Op = LD Int | ADD | MULT | DUP | DEC | SWAP | POP Int
+
+-- a program of stack operations
 type alias Prog = List Op
+
+-- a stack is a list of integers
 type alias Stack = List Int
 
+-- in case stack fails
 type alias D  = Stack -> (Maybe Stack)
 
+-- number os elements in the stack
 type alias Rank = Int
+
+-- rank of operation  (popped, pushed)
 type alias OpRank = (Int,Int)
 
 semOp : Op -> D
 semOp op stack = case op of 
+        -- push one element (i)
         LD i -> Just (i::stack)
+        -- pop two elements and push their sum
         ADD  -> case stack of
                  a::b::rs -> Just ((a+b)::rs)
                  _        -> Nothing
+        -- same general form as ADD
         MULT -> case stack of 
-                 -- same general form as ADD
                  a::b::rs -> Just ((a*b)::rs)
                  _        -> Nothing
         DUP  -> case stack of
@@ -39,7 +50,7 @@ semOp op stack = case op of
             case stack of
                 x :: y :: rest -> Just (y :: x :: rest)
                 _ -> Nothing
-
+        -- pop k elements from the stack
         POP k ->
             if List.length stack >= k then
                 Just (List.drop k stack)
@@ -51,7 +62,7 @@ semProg : Prog -> D
 semProg prog stack =
     case prog of
         [] ->
-            Just stack
+            Just stack -- base case, the stack is done
         -- recursively evaluates the operations and modifies stack
         op::rs ->
             case semOp op stack of
@@ -66,41 +77,51 @@ semProg prog stack =
 rankOp : Op -> OpRank
 rankOp op =
     case op of
-        LD _     -> (0, 1)
-        ADD      -> (2, 1)
-        MULT     -> (2, 1)
-        DUP      -> (1, 2)
-        DEC      -> (1, 1)
-        SWAP     -> (2, 2) 
-        POP k    -> (k, 0)
+        LD _     ->
+            (0, 1) -- pushed one item
+        ADD      ->
+            (2, 1) -- pop two elements, push one
+        MULT     ->
+            (2, 1) -- pop two elements, push one
+        DUP      ->
+            (1, 2) -- pop one element, push two
+        DEC      ->
+            (1, 1) -- pop one, and push one
+        SWAP     ->
+            (2, 2) -- pop two elements, and push two
+        POP k    ->
+            (k, 0) -- pop k elements
 
 rank : Prog -> Rank -> Maybe Rank
 rank prog r =
     case prog of
         [] ->
-            Just r
+            Just r -- base case, return the result
 
         op :: rest ->
             let
-                (n, m) = rankOp op
+                (n, m) = rankOp op -- n popped elements, pushed elements 
             in
             if r < n then
-                Nothing
+                Nothing -- less elemenets than allowed
             else
                 rank rest (r - n + m)
 
-
+-- make sure the program is well typed when start
 rankProg : Prog -> Maybe Rank
 rankProg prog =
     rank prog 0
 
 -- B
 
+-- if valid then runs the program on an empty stack.
 semTC : Prog -> Maybe Stack
 semTC prog =
     case rankProg prog of
-        Just _ -> semProg prog []
-        Nothing -> Nothing
+        Just _ ->
+            semProg prog []
+        Nothing -> 
+            Nothing
 
 -- 2. Shape Language
 
@@ -130,4 +151,27 @@ bbox shape =
 
 -- B
 -- Define a type checker that uses the following declaration
--- rect : Shape -> Maybe BBox
+rect : Shape -> Maybe BBox
+rect shape = 
+    case shape of
+        X ->
+            Just (1, 1) -- base case
+        LR shape1 shape2 ->
+            case (rect shape1, rect shape2) of
+                (Just (w1, h1), Just (w2, h2)) ->
+                    if h1 == h2 then -- both shapes are rectangular and same height
+                        Just (w1 + w2, h1) -- or h2
+                    else
+                        Nothing
+                _ ->
+                    Nothing
+
+        TB shape1 shape2 ->
+            case (rect shape1, rect shape2) of
+                (Just (w1, h1), Just (w2, h2)) ->
+                    if w1 == w2 then -- both shapes are rectangular and same width
+                        Just (w1, h1 + h2) -- or h1
+                    else
+                        Nothing
+                _ ->
+                    Nothing
