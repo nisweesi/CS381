8bcf4e5936f97493ec240d0e7c7caada25e5f778
mortonwi <135153158+mortonwi@users.noreply.github.com>
2025-05-31 13:17:39 -0700

    Add files via upload

--- hw/assignment6/hw6.pl
+++ hw/assignment6/hw6.pl
@@ -37,3 +37,24 @@ usage(Room, Time) :-
     when(Class, Time).
 
 % C
+conflict(Class1, Class2) :-
+    where(Class1, Place),
+    where(Class2, Place),
+    when(Class1, Time),
+    when(Class2, Time),
+    Class1 \= Class2.
+
+% D
+% Strategy is to define two predicates for both cases
+meet(Student1, Student2) :-
+    % Same class time and place
+    schedule(Student1, Place, Time),
+    schedule(Student2, Place, Time),
+    Student1 \= Student2.
+
+meet(Student1, Student2) :-
+    % Back to back classes in the same place
+    schedule(Student1, Place, Time1),
+    schedule(Student2, Place, Time2),
+    Student1 \= Student2,
+    Time2 is Time1 + 1.
