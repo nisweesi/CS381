d8ae131eaa3d62813298c5ac6c304cc8986d4446
mortonwi <135153158+mortonwi@users.noreply.github.com>
2025-05-31 12:35:34 -0700

    solved 1a 1b

--- hw/assignment6/hw6.pl
+++ hw/assignment6/hw6.pl
@@ -1,1 +1,39 @@
-% homework 6
+/* GROUP:
+    Nadir Isweesi
+    William Morton
+*/
+
+% Rules/Facts
+
+when(275,10).
+when(261,12).
+when(381,11).
+when(398,12).
+when(399,12).
+
+where(275,owen102).
+where(261,dear118).
+where(381,cov216).
+where(398,dear118).
+where(399,cov216).
+
+enroll(mary,275).
+enroll(john,275).
+enroll(mary,261).
+enroll(john,381).
+enroll(jim,399).
+
+% Excercise 1 Database Application
+
+% A
+schedule(Student, Place, Time) :-
+    enroll(Student, Class),
+    where(Class, Place),
+    when(Class, Time).
+
+% B
+usage(Room, Time) :-
+    where(Class, Room),
+    when(Class, Time).
+
+% C
